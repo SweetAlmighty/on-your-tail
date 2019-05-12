a7bba3d3c51989a88557458fead67022382ab85e
Brian <bsweet48@gmail.com>
2019-05-11 22:46:18 -0400

    Added Image. Image is set to mouse cursor position.

--- main.lua
+++ main.lua
@@ -1,9 +1,14 @@
 
 function love.load()
+    love.window.setTitle("Hugger")
+    love.graphics.setBackgroundColor(1, 1, 1);
+    image = love.graphics.newImage("Untitled.png")
 end
 
 function love.update(dt)
+    x, y = love.mouse.getPosition() 
 end
 
 function love.draw()
+    love.graphics.draw(image, x - 150, y - 150)
 end
