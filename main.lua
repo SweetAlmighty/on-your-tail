227269600fa193c3c008d0e9420ae1ccb6df567c
Brian <bsweet48@gmail.com>
2019-06-12 21:06:51 -0400

    Cleaned up some functions, removed unnecessary code. Moved temp buildings to side of screen. Clamped player position to "street".

--- main.lua
+++ main.lua
@@ -1,9 +1,53 @@
-
+-- LOVE2D Functions
 function love.load()
-    love.window.setTitle("On Your Tail")
+    initializeWindow()
+    initializeScene()
+    initializeKeymapping()
+end
 
+function love.update(dt)
+    processInput(dt)
+    processSceneMovement(dt);
+end
+
+function love.draw()
+    love.graphics.setColor(1, 1, 0)
+    for i,b in ipairs(buildings) do
+        love.graphics.rectangle("fill", b.x, b.y, b.width, b.height)
+    end
+
+    love.graphics.setColor(0, 0, 1)
+    love.graphics.circle("fill", player.x, player.y, player.radius)
+end
+--
+
+
+function initializeWindow()
     love.window.setMode(320, 240)
+    love.window.setTitle("On Your Tail")
+    love.graphics.setBackgroundColor(0.5, 0.5, 0.5)
+end
 
+function initializeScene()
+    player = {}
+    player.x = 160
+    player.y = 120
+    player.speed = 180
+    player.radius = 10
+
+    street = {}
+    street.XMin = 50
+    street.YMin = 0
+    street.XMax = 270
+    street.YMax = 240
+    
+    buildings = {}
+    sceneSpeed = 180
+
+    spawnBuildings()
+end
+
+function initializeKeymapping()
     inputKeyMap = 
     {
         up = "up",
@@ -15,67 +59,64 @@ function love.load()
 
     inputActionMap =
     {
-        [inputKeyMap.up] = function(x) player.y = player.y - x end,
-        [inputKeyMap.left] = function(x) player.x = player.x - x end,
-        [inputKeyMap.down] = function(x) player.y = player.y + x end,
-        [inputKeyMap.right] = function(x) player.x = player.x + x end,
-        [inputKeyMap.exit] = function(x) love.event.quit() end
+        [inputKeyMap.up]    = function(x) clampPlayerY(player.y - x) end,
+        [inputKeyMap.left]  = function(x) clampPlayerX(player.x - x) end,
+        [inputKeyMap.down]  = function(x) clampPlayerY(player.y + x) end,
+        [inputKeyMap.right] = function(x) clampPlayerX(player.x + x) end,
+        [inputKeyMap.exit]  = function(x) love.event.quit() end
     }
+end
 
-    player = {}
-    player.x = 0
-    player.y = 0
-    player.speed = 180
-
-    buildings = {}
-    sceneSpeed = 180
-
-    for i = 0, 4, 1 do
-        spawnBuilding()
+function spawnBuildings()
+    for i = 0, 5, 1 do
+        building = {}
+        building.x = (i <= 2) and 0 or 270
+        building.y = ((i <= 2) and i or i - 3) * -120
+        building.width = 50
+        building.height = 100
+        table.insert(buildings, building)
     end
-end
-
-function love.update(dt)
-    processInput(dt)
-    processSceneMovement(dt);
-end
-
-function love.draw()
-    for i,b in ipairs(buildings) do
-        love.graphics.rectangle("fill", b.x, b.y, b.width, b.height)
-    end
-
-    love.graphics.circle("line", player.x, player.y, 10)
-end
-
-function spawnBuilding()
-    building = {}
-    building.x = math.random(0, love.graphics.getWidth())
-    building.y = math.random(0, love.graphics.getHeight())
-    building.width = math.random(50, 200)
-    building.height = math.random(50, 200)
-
-    table.insert(buildings, building)
 end
 
 function processInput(dt)
     for k, v in pairs(inputKeyMap) do
         if love.keyboard.isDown(v) then
             inputActionMap[v](player.speed * dt)
-
-            -- Debug scenario to test out "ternary" operation
-            print("In Bounds: " .. ((player.x > 0 and player.y > 0) and "Yes" or "No"))
         end
     end
+end
+
+function clampPlayerX(pos) 
+    local newPos = pos
+
+    if pos < street.XMin + player.radius then
+        newPos = street.XMin + player.radius
+    elseif pos > street.XMax - player.radius then
+        newPos = street.XMax - player.radius
+    end
+
+    player.x = newPos
+end
+
+function clampPlayerY(pos)
+    local newPos = pos
+
+    if pos < street.YMin + player.radius then
+        newPos = street.YMin + player.radius
+    elseif pos > street.YMax - player.radius then
+        newPos = street.YMax - player.radius
+    end
+
+    player.y = newPos
 end
 
 function processSceneMovement(dt)
     for i,b in ipairs(buildings) do
         b.y = b.y + sceneSpeed * dt
-        
+
         if b.y > love.graphics.getHeight() then
-            b.x = math.random(0, love.graphics.getWidth())
-            b.y = math.random(-love.graphics.getHeight(), 0)
+            b.x = (i > 3) and 0 or 270
+            b.y = -120
         end
     end
 end
