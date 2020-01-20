914d3dd6fe8074975cc6879fea49bfcce14d3f67
Brian Sweet <bsweet48@gmail.com>
2020-01-20 15:32:46 -0500

    Fixed error with null ref on randomPosition

--- src/entities/kitten.lua
+++ src/entities/kitten.lua
@@ -3,13 +3,18 @@ require "src/entities/cat"
 
 Kitten = class("Kitten", Cat)
 
+function randomPosition(entity)
+    return { love.math.random(screenWidth - entity.spriteWidth, screenWidth * 2),
+        love.math.random(playableArea.y - entity.spriteHeight, playableArea.height) }
+end
+
 function Kitten:initialize()
     Entity.initialize(self, Types.KITTEN, 1)
 
     Entity.setState(self, e_States.IDLE)
     Entity.setDirection(self, Directions.E)
     Entity.setImageDefaults(self, 102, 90, 17, 15)
-    Entity.setPosition(self, Entity.randomPosition(self))
+    Entity.setPosition(self, randomPosition(self))
 
     local animats = animatFactory:createWithLayer("kitten", lume.randomchoice(catType))
     Entity.setAnims(self, { animats[2], animats[1], animats[2], animats[3] })
